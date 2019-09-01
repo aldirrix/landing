6844d8ca01f348bb5ace366539b0e40c8fc7abb6
Aldo Preciado <a.cuevas@borderguru.com>
2019-09-01 20:30:48 +0200

    initial configuration

---
+++ src/components/About.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About: React.FunctionComponent = () => <h1>Que pedsso</h1>;
+
+export default About;
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import About from './components/About';
+
+ReactDOM.render(<About />, document.getElementById('app'));
